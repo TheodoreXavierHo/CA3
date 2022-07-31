8494d08fb5b32d2a330b1634a85b7c9cde654a55
Theodore <theodorexavierho@gmail.com>
2022-07-31 20:52:50 +0800

    0.01

--- src/Assessment.java
+++ src/Assessment.java
@@ -6,7 +6,60 @@ public class Assessment {
     private double totalMarks; // Total possible marks for this Assessment
     private double weightage; // The percentage of marks (out of the overall) that this Assessment carries
 
+    public Assessment(String name, String description, double totalMarks) {
+        this.name = name;
+        this.description = description;
+        this.totalMarks = totalMarks;
+    }
     public double getWeightedMarks() {
         return this.marks/this.totalMarks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module = module;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public double getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(double totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    public double getWeightage() {
+        return weightage;
+    }
+
+    public void setWeightage(double weightage) {
+        this.weightage = weightage;
     }
 }
--- src/Module.java
+++ src/Module.java
@@ -5,20 +5,22 @@ public class Module {
     private String moduleCode; // The module code of the Module
     private String description; // The description of the Module
     private int creditUnits; // How many credits the Module carries
-
     // A list of Assessments that the module uses to determine the final grade of the student (e.g. CA1, CA2, CA3 etc.)
-    private ArrayList<Assessment> assessments;
+    private ArrayList<Assessment> assessments = new ArrayList<>();
 
-    public void listTest() {
-        this.assessments.add();
-
-        for (int x = 0; x <= assessments.size(); x++) {
-            System.out.println(assessments.get(x));
-        }
-    }
 
     public static void main(String[] args) {
         Module test = new Module();
-        test.listTest();
+        test.assessments.add(new Assessment("CA1", "Grade" , 120));
+        test.assessments.add(new Assessment("CA2", "Grad3" , 122130));
+        test.assessments.add(new Assessment("CA3", "Graded" , 1212));
+
+
+
+        //System.out.println(test.assessments);
+        test.assessments.forEach(assessment -> {
+            System.out.println(assessment.getName() + " - " + assessment.getDescription()
+                    + " - " + assessment.getTotalMarks());
+        });
     }
 }
